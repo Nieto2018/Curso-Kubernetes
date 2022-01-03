523286a7917cb6e40630a12d054c3653b8c52812
Antonio Nieto García <antonio.nieto.garcia@externos.gloval.es>
2022-01-03 09:21:43 +0100

    Cluster autoscaler

--- AWS/Cluster Autoscaler/199/commands.sh
+++ AWS/Cluster Autoscaler/199/commands.sh
@@ -1,8 +1,8 @@
 aws eks describe-cluster --name test-cluster --query "cluster.identity.oidc.issuer" --output text
-> https://oidc.eks.us-east-1.amazonaws.com/id/EE92B106C488C3982F1394C1957FE758
+> https://oidc.eks.us-east-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B716D3041E
 
-aws iam list-open-id-connect-providers | grep EE92B106C488C3982F1394C1957FE758
-> "Arn": "arn:aws:iam::898493283539:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/EE92B106C488C3982F1394C1957FE758"
+aws iam list-open-id-connect-providers | grep EXAMPLED539D4633E53DE1B716D3041E
+> "Arn": "arn:aws:iam::111122223333:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/EXAMPLED539D4633E53DE1B716D3041E"
 
 # ---------------------------------------------
 
@@ -13,8 +13,8 @@ aws iam create-policy \
   {
       "Policy": {
           "PolicyName": "AmazonEKSClusterAutoscalerPolicy",
-          "PolicyId": "ANPA5CMS7PTJ2B4SAESVT",
-          "Arn": "arn:aws:iam::898493283539:policy/AmazonEKSClusterAutoscalerPolicy",
+          "PolicyId": "ANDA5CMS7PTJ2B4SAESZT",
+          "Arn": "arn:aws:iam::111122223333:policy/AmazonEKSClusterAutoscalerPolicy",
           "Path": "/",
           "DefaultVersionId": "v1",
           "AttachmentCount": 0,
@@ -30,17 +30,17 @@ eksctl create iamserviceaccount \
   --cluster=test-cluster \
   --namespace=kube-system \
   --name=cluster-autoscaler \
-  --attach-policy-arn=arn:aws:iam::898493283539:policy/AmazonEKSClusterAutoscalerPolicy \
+  --attach-policy-arn=arn:aws:iam::111122223333:policy/AmazonEKSClusterAutoscalerPolicy \
   --override-existing-serviceaccounts \
   --approve
 
 kubectl annotate serviceaccount cluster-autoscaler \
   -n kube-system \
-  eks.amazonaws.com/role-arn=arn:aws:iam::898493283539:role/eksctl-test-cluster-nodegroup-tes-NodeInstanceRole-16RL3OM9REARW
+  eks.amazonaws.com/role-arn=arn:aws:iam::111122223333:role/eksctl-test-cluster-nodegroup-tes-NodeInstanceRole-16RL34M9REARW
 
 kubectl annotate serviceaccount cluster-autoscaler \
   -n kube-system \
-  eks.amazonaws.com/role-arn=arn:aws:iam::898493283539:role/AWSServiceRoleForAutoScaling
+  eks.amazonaws.com/role-arn=arn:aws:iam::111122223333:role/AWSServiceRoleForAutoScaling
 
 kubectl set image deployment cluster-autoscaler \
   -n kube-system \
@@ -48,8 +48,8 @@ kubectl set image deployment cluster-autoscaler \
 
   0102 12:31:31.394312       1 aws_manager.go:265] Failed to regenerate ASG cache: cannot autodiscover ASGs: WebIdentityErr: failed to retrieve credentials
 caused by: AccessDenied: Not authorized to perform sts:AssumeRoleWithWebIdentity
-	status code: 403, request id: 59c595b8-87d1-40a8-9797-a6e71836b439
+	status code: 403, request id: 59c595b8-87d3-40a8-9797-a6e71836b439
 F0102 12:31:31.394342       1 aws_cloud_provider.go:389] Failed to create AWS Manager: cannot autodiscover ASGs: WebIdentityErr: failed to retrieve credentials
 caused by: AccessDenied: Not authorized to perform sts:AssumeRoleWithWebIdentity
-	status code: 403, request id: 59c595b8-87d1-40a8-9797-a6e71836b439
+	status code: 403, request id: 59c595b8-89d1-40a8-9797-a6e71836b439
 goroutine 65 [running]:
